ced595b98bc1f7606cc75e6b792b87ef790ece72
Hưng Bảo <vmhsky@gmail.com>
2022-08-19 10:53:24 +0700

    D

--- src/containers/Admin/Account/AccountContainerAdmin.jsx
+++ src/containers/Admin/Account/AccountContainerAdmin.jsx
@@ -5,8 +5,11 @@ import { useDispatch, useSelector } from "react-redux";
 import AccountComponentAdmin from "../../../components/Admin/Account/AccountComponentAdmin";
 import { setAccount } from "../../../features/account/account";
 import { getCookie } from "../../../helpers/cookie.helper";
+import {useNavigate} from "react-router-dom"
+import { redirectError } from "../../../helpers/redirectError";
 export const ObjContext = createContext();
 export default function AccountContainerAdmin() {
+  const navigate = useNavigate();
   const alert = useAlert();
   const [accountRegister, setAccountRegister] = React.useState({
     username: "",
@@ -30,16 +33,24 @@ export default function AccountContainerAdmin() {
   }, []);
 
   const getAllAccount = async () => {
+   try {
     const result = await axios.get(
       `${import.meta.env.VITE_BACKEND_SITE}/accounts`,
       {
         headers: {
           "Content-type": "application/json; charset=UTF-8",
           email: getCookie("email"),
+          Authorization: `Bearer ${getCookie("access_token")}`
         },
       }
     );
     return result.data;
+   } catch (error) {
+    const {status}  = error.response;
+    if(status === 401) {
+      return navigate("/login")
+    }
+   }
   };
   const handleChange = (e) => {
     const { name, value } = e.target;
