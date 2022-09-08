293f6a5513d59d4c103bf76cc635db5caf443ceb
nguyentrieutien1 <nguyenthanhtung111xxx@gmail.com>
2022-09-08 12:48:14 +0700

    D

--- src/components/Admin/Account/Form/FormComponent.jsx
+++ src/components/Admin/Account/Form/FormComponent.jsx
@@ -3,7 +3,6 @@ import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { InputLabel, MenuItem, Select } from "@mui/material";
@@ -42,13 +41,9 @@ export default function FormComponent(props) {
               <Grid item xs={6}>
                 <TextField
                   margin="normal"
-                  required
                   fullWidth
-                  id="username"
                   label="Enter Username"
                   name="username"
-                  autoComplete="username"
-                  autoFocus
                   onChange={handleChange}
                   value={username || ""}
                 />
@@ -56,13 +51,9 @@ export default function FormComponent(props) {
               <Grid item xs={6}>
                 <TextField
                   margin="normal"
-                  required
                   fullWidth
-                  id="email"
                   label="Enter Email"
                   name="email"
-                  autoComplete="email"
-                  autoFocus
                   onChange={handleChange}
                   value={email || ""}
                 />
@@ -70,13 +61,9 @@ export default function FormComponent(props) {
               <Grid item xs={6}>
                 <TextField
                   margin="normal"
-                  required
                   fullWidth
-                  id="address"
                   label="Enter Address"
                   name="address"
-                  autoComplete="address"
-                  autoFocus
                   onChange={handleChange}
                   value={address || ""}
                 />
@@ -86,8 +73,6 @@ export default function FormComponent(props) {
                 <InputLabel id="demo-simple-select-label">Sex</InputLabel>
                 <Select
                   fullWidth
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
                   label="sex"
                   name="sex"
                   onChange={handleChange}
@@ -104,8 +89,6 @@ export default function FormComponent(props) {
                 <InputLabel id="demo-simple-select-label">Role</InputLabel>
                 <Select
                   fullWidth
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
                   label="role"
                   name="role"
                   onChange={handleChange}
@@ -120,14 +103,10 @@ export default function FormComponent(props) {
               <Grid item xs={6}>
                 <TextField
                   margin="normal"
-                  required
                   fullWidth
-                  id="password"
                   label="Enter Password"
                   name="password"
-                  autoComplete="password"
                   type="password"
-                  autoFocus
                   onChange={handleChange}
                   value={password || ""}
                 />
@@ -135,13 +114,10 @@ export default function FormComponent(props) {
               <Grid item xs={6}>
                 <TextField
                   margin="normal"
-                  required
                   fullWidth
-                  id="comfirmPassword"
                   label="Enter ComfirmPassword"
                   name="comfirmPassword"
                   autoComplete="comfirmPassword"
-                  autoFocus
                   onChange={handleChange}
                   type="password"
                   value={comfirmPassword || ""}
